869418447cfc3b4c0c6b3210c3e7537eeb8627ff
Michael Gagnon <mikegagnon@gmail.com>
2016-06-07 14:33:32 -0600

    Clean whitespace

--- src/main/scala/robobot/webapp/Viz.scala
+++ src/main/scala/robobot/webapp/Viz.scala
@@ -11,7 +11,6 @@ class Viz(val config: Config) {
   jQuery("#" + config.mainDivId).append(s"<svg id='${config.svgId}'></svg>")
 
   val svg = jQuery("#" + config.svgId)
-
   svg.attr("xmlns", "'http://www.w3.lrg/2000/svg'")
   svg.attr("width", config.svgWidth)
   svg.attr("height", config.svgHeight)
