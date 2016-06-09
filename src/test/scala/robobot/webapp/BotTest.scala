5788c785d177eee6e6558848eaeed273f015275a
Michael Gagnon <mikegagnon@gmail.com>
2016-06-09 16:29:14 -0600

    More thorough testing for Bot.cycle()

--- src/test/scala/robobot/webapp/BotTest.scala
+++ src/test/scala/robobot/webapp/BotTest.scala
@@ -40,13 +40,9 @@ object BotTest extends TestSuite {
         board.addBot(bot1)
 
         board.matrix(0)(0) ==> Some(bot1)
-
         Range(0, config.moveCycles -1).foreach { (_) => bot1.cycle() }
-
         board.matrix(0)(0) ==> Some(bot1)
-
         bot1.cycle()
-
         board.matrix(0)(0) ==> None
         board.matrix(0)(1) ==> Some(bot1)
       }
@@ -63,13 +59,9 @@ object BotTest extends TestSuite {
         board.addBot(bot1)
 
         bot1.direction ==> Direction.Right
-
         Range(0, config.turnCycles -1).foreach { (_) => bot1.cycle() }
-
         bot1.direction ==> Direction.Right
-
         bot1.cycle()
-
         bot1.direction ==> Direction.Up
       }
 
@@ -86,30 +78,63 @@ object BotTest extends TestSuite {
         board.addBot(bot1)
 
         // Turn instruction
-
         bot1.direction ==> Direction.Right
-
         Range(0, config.turnCycles -1).foreach { (_) => bot1.cycle() }
-
         bot1.direction ==> Direction.Right
-
         bot1.cycle()
-
         bot1.direction ==> Direction.Down
 
         // Move instruction
-
         board.matrix(0)(0) ==> Some(bot1)
-
         Range(0, config.moveCycles -1).foreach { (_) => bot1.cycle() }
-
         board.matrix(0)(0) ==> Some(bot1)
-
         bot1.cycle()
+        board.matrix(0)(0) ==> None
+        board.matrix(1)(0) ==> Some(bot1)
+      }
 
+      "turn instruction followed by move instruction, and then repeat"-{
+
+        val board = new Board()
+
+        // A bot with only the turn instruction instruction
+        val bot1 = Bot(board, 0, 0)
+        bot1.direction = Direction.Right
+        val bank0 = new Bank()
+        bank0.instructions ++= TurnInstruction(1) :: MoveInstruction() :: Nil
+
+        bot1.banks += (0 -> bank0)
+        board.addBot(bot1)
+
+        // Turn instruction
+        bot1.direction ==> Direction.Right
+        Range(0, config.turnCycles -1).foreach { (_) => bot1.cycle() }
+        bot1.direction ==> Direction.Right
+        bot1.cycle()
+        bot1.direction ==> Direction.Down
+
+        // Move instruction
+        board.matrix(0)(0) ==> Some(bot1)
+        Range(0, config.moveCycles -1).foreach { (_) => bot1.cycle() }
+        board.matrix(0)(0) ==> Some(bot1)
+        bot1.cycle()
         board.matrix(0)(0) ==> None
         board.matrix(1)(0) ==> Some(bot1)
 
+        // Turn instruction
+        bot1.direction ==> Direction.Down
+        Range(0, config.turnCycles -1).foreach { (_) => bot1.cycle() }
+        bot1.direction ==> Direction.Down
+        bot1.cycle()
+        bot1.direction ==> Direction.Left
+
+        // Move instruction
+        board.matrix(1)(0) ==> Some(bot1)
+        Range(0, config.moveCycles -1).foreach { (_) => bot1.cycle() }
+        board.matrix(1)(0) ==> Some(bot1)
+        bot1.cycle()
+        board.matrix(1)(0) ==> None
+        board.matrix(1)(config.numCols - 1) ==> Some(bot1)
       }
     }
   }
