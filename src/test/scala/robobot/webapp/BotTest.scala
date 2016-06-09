b9936e4d9c53935aa425c219fcc2cc2c5ec19080
Michael Gagnon <mikegagnon@gmail.com>
2016-06-09 16:18:56 -0600

    Test cycle with TurnInstruction

--- src/test/scala/robobot/webapp/BotTest.scala
+++ src/test/scala/robobot/webapp/BotTest.scala
@@ -26,28 +26,54 @@ object BotTest extends TestSuite {
       }
     }
 
+    // TODO: factor out common code
     "cycle"-{
-      val board = new Board()
+      "one move instruction"-{
+        val board = new Board()
 
-      // A bot with only the move instruction
-      val bot1 = Bot(board, 0, 0)
-      bot1.direction = Direction.Right
-      val bank0 = new Bank()
-      bank0.instructions :+= MoveInstruction()
-      bot1.banks += (0 -> bank0)
-      board.addBot(bot1)
+        // A bot with only the move instruction
+        val bot1 = Bot(board, 0, 0)
+        bot1.direction = Direction.Right
+        val bank0 = new Bank()
+        bank0.instructions :+= MoveInstruction()
+        bot1.banks += (0 -> bank0)
+        board.addBot(bot1)
 
-      board.matrix(0)(0) ==> Some(bot1)
+        board.matrix(0)(0) ==> Some(bot1)
 
-      Range(0, config.moveCycles -1).foreach { (_) => bot1.cycle() }
+        Range(0, config.moveCycles -1).foreach { (_) => bot1.cycle() }
 
-      board.matrix(0)(0) ==> Some(bot1)
+        board.matrix(0)(0) ==> Some(bot1)
 
-      bot1.cycle()
+        bot1.cycle()
 
-      board.matrix(0)(0) ==> None
-      board.matrix(0)(1) ==> Some(bot1)
+        board.matrix(0)(0) ==> None
+        board.matrix(0)(1) ==> Some(bot1)
+      }
 
+      "one turn instruction"-{
+        val board = new Board()
+
+        // A bot with only the turn instruction instruction
+        val bot1 = Bot(board, 0, 0)
+        bot1.direction = Direction.Right
+        val bank0 = new Bank()
+        bank0.instructions :+= TurnInstruction(0)
+        bot1.banks += (0 -> bank0)
+        board.addBot(bot1)
+
+        bot1.direction ==> Direction.Right
+
+        Range(0, config.turnCycles -1).foreach { (_) => bot1.cycle() }
+
+        bot1.direction ==> Direction.Right
+
+        bot1.cycle()
+
+        bot1.direction ==> Direction.Up
+
+
+      }
     }
   }
 }
