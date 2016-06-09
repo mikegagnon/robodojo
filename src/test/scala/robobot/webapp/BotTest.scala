f8e6533acf318f6fc89cbdf380c519c083a91fa7
Michael Gagnon <mikegagnon@gmail.com>
2016-06-09 16:24:19 -0600

    Test for bot.cycle() with TurnInstruction followed by MoveInstruction

--- src/test/scala/robobot/webapp/BotTest.scala
+++ src/test/scala/robobot/webapp/BotTest.scala
@@ -71,7 +71,44 @@ object BotTest extends TestSuite {
         bot1.cycle()
 
         bot1.direction ==> Direction.Up
+      }
 
+      "turn instruction followed by move instruction"-{
+        val board = new Board()
+
+        // A bot with only the turn instruction instruction
+        val bot1 = Bot(board, 0, 0)
+        bot1.direction = Direction.Right
+        val bank0 = new Bank()
+        bank0.instructions ++= TurnInstruction(1) :: MoveInstruction() :: Nil
+
+        bot1.banks += (0 -> bank0)
+        board.addBot(bot1)
+
+        // Turn instruction
+
+        bot1.direction ==> Direction.Right
+
+        Range(0, config.turnCycles -1).foreach { (_) => bot1.cycle() }
+
+        bot1.direction ==> Direction.Right
+
+        bot1.cycle()
+
+        bot1.direction ==> Direction.Down
+
+        // Move instruction
+
+        board.matrix(0)(0) ==> Some(bot1)
+
+        Range(0, config.moveCycles -1).foreach { (_) => bot1.cycle() }
+
+        board.matrix(0)(0) ==> Some(bot1)
+
+        bot1.cycle()
+
+        board.matrix(0)(0) ==> None
+        board.matrix(1)(0) ==> Some(bot1)
 
       }
     }
