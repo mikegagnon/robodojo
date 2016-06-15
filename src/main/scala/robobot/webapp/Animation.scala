bb4fcdb96deae71664a5ea69f2482615eed5cef5
Michael Gagnon <mikegagnon@gmail.com>
2016-06-14 18:47:06 -0600

    Improve documentation for Animation

--- src/main/scala/robobot/webapp/Animation.scala
+++ src/main/scala/robobot/webapp/Animation.scala
@@ -1,14 +1,13 @@
 package robobot.webapp
 
 // The Animation classes provide just information to Viz so that Viz can animate the instruction
-// execution.
+// execution. When an Instruction executes a cycle, it returns an optional Animation object.
 
 sealed trait Animation {
   val botId: Long
   val cycleNum: Int
 }
 
-// TODO: move cycleNum to below botId
 case class MoveAnimation(
   botId: Long,
   cycleNum: Int,
