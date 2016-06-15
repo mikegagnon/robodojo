19b61ec9a73a0322dd37d447f8efe32727662404
Michael Gagnon <mikegagnon@gmail.com>
2016-06-15 11:54:43 -0600

    Add untested strawman compiler

--- src/main/scala/robobot/webapp/Compiler.scala
+++ src/main/scala/robobot/webapp/Compiler.scala
@@ -14,6 +14,12 @@ object Compiler {
 
   }
 
+  case class CompileTokenLineResult(
+    instruction: Option[Instruction],
+    // Some((description of error, line number))
+    errorMessage: Option[(String, Int)])
+
+  // TODO: filter out name, author, and country
   def tokenize(text: String): Array[TokenLine] =
     text
       .split("\n")
@@ -35,7 +41,33 @@ object Compiler {
         tokens.nonEmpty
       }
 
+  // TODO: test
   def compile(text: String): Map[Int, Bank] = {
+
+    val lines: Array[TokenLine] = tokenize(text)
+    var bankNumber = 0
+
+    def compileBank(tl: TokenLine): CompileTokenLineResult =
+      if (tl.tokens.length > 2) {
+        return CompileTokenLineResult(None,
+          Some(("bank directive takes only one optional parameter", tl.lineNumber)))
+      } else {
+          bankNumber += 1
+          return CompileTokenLineResult(None, None)
+      }
+
+    def unrecognizedInstruction(tl: TokenLine) =
+      CompileTokenLineResult(None,
+          Some((s"Unrecognized instruction name: ${tl.tokens(0)}", tl.lineNumber)))
+
+    // TODO: very friendly error messages
+    lines.foreach { case (tl: TokenLine) =>
+
+      val result: CompileTokenLineResult = tl.tokens(0) match {
+        case "bank" => compileBank(tl)
+        case _ => unrecognizedInstruction(tl)
+      }
+    }
 
     null
   }
