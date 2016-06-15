6829d0785877c8ef520c66a11496714a227f0795
Michael Gagnon <mikegagnon@gmail.com>
2016-06-15 09:52:38 -0600

    Add strawman tests for Compiler

--- src/main/scala/robobot/webapp/Compiler.scala
+++ src/main/scala/robobot/webapp/Compiler.scala
@@ -2,7 +2,9 @@ package robobot.webapp
 
 object Compiler {
 
-  def tokenize(text: String): Array[Array[String]] = {
+  case class TokenLine(tokens: Array[String], lineNumber: Int)
+
+  def tokenize(text: String): Array[TokenLine] =
     text
       .split("\n")
       // Remove comments
@@ -10,11 +12,18 @@ object Compiler {
       // Separate into tokens
       .map { line: String => line.split("""\s+""") }
       // Drop empty tokens
-      .map { line: Array[String] =>
-        line.filter { _ != "" }
+      .map { tokens: Array[String] =>
+        tokens.filter { _ != "" }
       }
-
-  }
+      // Pair lines with line numbers
+      .zipWithIndex
+      .map { case (tokens: Array[String], lineNumber: Int) =>
+        TokenLine(tokens, lineNumber)
+      }
+      // Drop empty lines
+      .filter { case TokenLine(tokens, lineNumber) =>
+        tokens.nonEmpty
+      }
 
   def compile(text: String): Map[Int, Bank] = {
 
--- src/test/scala/robobot/webapp/CompilerTest.scala
+++ src/test/scala/robobot/webapp/CompilerTest.scala
@@ -4,26 +4,65 @@ import utest._
 
 object CompilerTest extends TestSuite {
 
+  import Compiler._
+
+  implicit class EnrichedTokenLine(tl: TokenLine) {
+    
+    def prettyString(): String = {
+      val tokens = tl.tokens.mkString(",")
+      s"TokenLine([${tokens}], ${tl.lineNumber})"
+    }
+
+    // TODO: test
+    def deepEquals(that: Any) =
+      that match {
+        case thatTl: TokenLine => {
+          tl.tokens.sameElements(thatTl.tokens) && tl.lineNumber == thatTl.lineNumber
+        }
+        case _ => false
+      }
+  }
+
+  // TODO: test
+  def tokenLinesEquals(tls1: Array[TokenLine], tls2: Array[TokenLine]) =
+    tls1.length == tls2.length &&
+    tls1.zip(tls2).forall{ case (a:  TokenLine, b: TokenLine) =>
+      a.deepEquals(b)
+    }
+
 
   val tests = this {
 
     "tokenize"-{
 
-      val text = """
+      "split empty text into zero lines" - {
+        val text = ""
+        val result = Compiler.tokenize(text)
+        val expectedResult = Array[TokenLine]()
+        assert(tokenLinesEquals(result, expectedResult))
+      }
 
-      a   b    c d ; foo    
+      "split text into one line" - {
+        val text = "1"
+        val result = Compiler.tokenize(text)
+        val expectedResult = Array(TokenLine(Array("1"), 0))
+        assert(tokenLinesEquals(result, expectedResult))
+      }
 
+      "split three lines" - {
+        val text =
+"""1
+2
+3"""
+        val result = Compiler.tokenize(text)
+        val expectedResult =
+          Array(
+            TokenLine(Array("1"), 0),
+            TokenLine(Array("2"), 1),
+            TokenLine(Array("3"), 2))
 
-      x     y   x   ;
-
-
-      """
-
-      Compiler.tokenize(text)
-        .foreach { line: Array[String] =>
-          println(line.mkString(","))
-        }
-
+        assert(tokenLinesEquals(result, expectedResult))
+      }
     }
   }
 }
