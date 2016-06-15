4d30285b07c167860be550c5884ff716de92fd31
Michael Gagnon <mikegagnon@gmail.com>
2016-06-15 10:14:01 -0600

    Moved EnrichedTokenLine code into TokenLine, since it's more elegant that way

--- src/main/scala/robobot/webapp/Compiler.scala
+++ src/main/scala/robobot/webapp/Compiler.scala
@@ -2,7 +2,17 @@ package robobot.webapp
 
 object Compiler {
 
-  case class TokenLine(tokens: Array[String], lineNumber: Int)
+  case class TokenLine(tokens: Array[String], lineNumber: Int) {
+
+    override def equals(that: Any): Boolean =
+      that match {
+        case tl: TokenLine => tokens.sameElements(tl.tokens) && lineNumber == tl.lineNumber
+        case _ => false
+      }
+
+    override def toString(): String = s"TokenLine([${tokens.mkString(",")}], ${lineNumber})"
+
+  }
 
   def tokenize(text: String): Array[TokenLine] =
     text
--- src/test/scala/robobot/webapp/CompilerTest.scala
+++ src/test/scala/robobot/webapp/CompilerTest.scala
@@ -6,51 +6,26 @@ object CompilerTest extends TestSuite {
 
   import Compiler._
 
-  // TODO: abort Enriched class
-  implicit class EnrichedTokenLine(tl: TokenLine) {
-    
-    def prettyString(): String = {
-      val tokens = tl.tokens.mkString(",")
-      s"TokenLine([${tokens}], ${tl.lineNumber})"
-    }
-
-    def deepEquals(that: Any) =
-      that match {
-        case thatTl: TokenLine => {
-          tl.tokens.sameElements(thatTl.tokens) && tl.lineNumber == thatTl.lineNumber
-        }
-        case _ => false
-      }
-  }
-
-  // TODO: test
-  def tokenLinesEquals(tls1: Array[TokenLine], tls2: Array[TokenLine]) =
-    tls1.length == tls2.length &&
-    tls1.zip(tls2).forall{ case (a:  TokenLine, b: TokenLine) =>
-      a.deepEquals(b)
-    }
-
-
   val tests = this {
 
-    "deepEquals"-{
+    "TokenLine.equals"-{
       "simple case"-{
         "equals"-{
           val a = TokenLine(Array(), 0)
           val b = TokenLine(Array(), 0)
-          assert(a.deepEquals(b))
+          a ==> b
         }
 
         "unequal lineNumbers"-{
           val a = TokenLine(Array(), 0)
           val b = TokenLine(Array(), 1)
-          assert(!a.deepEquals(b))
+          assert(a != b)
         }
 
         "unequal token array"-{
           val a = TokenLine(Array(), 0)
           val b = TokenLine(Array("a"), 0)
-          assert(!a.deepEquals(b))
+          assert(a != b)
         }
       }
 
@@ -58,13 +33,13 @@ object CompilerTest extends TestSuite {
         "equals"-{
           val a = TokenLine(Array("a"), 1)
           val b = TokenLine(Array("a"), 1)
-          assert(a.deepEquals(b))
+          a ==> b
         }
 
         "unequal token array"-{
           val a = TokenLine(Array("b"), 0)
           val b = TokenLine(Array("a"), 0)
-          assert(!a.deepEquals(b))
+          assert(a != b)
         }
       }
 
@@ -72,12 +47,12 @@ object CompilerTest extends TestSuite {
         "equals"-{
           val a = TokenLine(Array("a", "b"), 2)
           val b = TokenLine(Array("a", "b"), 2)
-          assert(a.deepEquals(b))
+          a ==> b
         }
         "not equals"-{
           val a = TokenLine(Array("a", "b"), 2)
           val b = TokenLine(Array("a", "c"), 2)
-          assert(!a.deepEquals(b))
+          assert(a != b)
         }
       }
 
@@ -89,14 +64,14 @@ object CompilerTest extends TestSuite {
         val text = ""
         val result = Compiler.tokenize(text)
         val expectedResult = Array[TokenLine]()
-        assert(tokenLinesEquals(result, expectedResult))
+        assert(result.sameElements(expectedResult))
       }
 
       "split text into one line" - {
         val text = "1"
         val result = Compiler.tokenize(text)
         val expectedResult = Array(TokenLine(Array("1"), 0))
-        assert(tokenLinesEquals(result, expectedResult))
+        assert(result.sameElements(expectedResult))
       }
 
       "split three lines" - {
@@ -111,7 +86,7 @@ object CompilerTest extends TestSuite {
             TokenLine(Array("2"), 1),
             TokenLine(Array("3"), 2))
 
-        assert(tokenLinesEquals(result, expectedResult))
+        assert(result.sameElements(expectedResult))
       }
     }
   }
