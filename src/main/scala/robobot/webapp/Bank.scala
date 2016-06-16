63906c32a337cf06eb8d10a2db1ad8742eee7d24
Michael Gagnon <mikegagnon@gmail.com>
2016-06-16 16:30:38 -0600

    Remove obsolete TODO comment

--- src/main/scala/robobot/webapp/Bank.scala
+++ src/main/scala/robobot/webapp/Bank.scala
@@ -4,5 +4,4 @@ import scala.collection.mutable.ArrayBuffer
 
 case class Bank(var instructions: ArrayBuffer[Instruction] = ArrayBuffer[Instruction]())
 
-// TODO: replace instances of banks with Program
 case class Program(var banks: Map[Int, Bank])
