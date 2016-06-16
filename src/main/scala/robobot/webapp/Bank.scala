3451ec38ee9ae2ec84b0da72541bea9b2b31dfc8
Michael Gagnon <mikegagnon@gmail.com>
2016-06-16 15:45:44 -0600

    rm obsolete TODO

--- src/main/scala/robobot/webapp/Bank.scala
+++ src/main/scala/robobot/webapp/Bank.scala
@@ -2,8 +2,6 @@ package robobot.webapp
 
 import scala.collection.mutable.ArrayBuffer
 
-
-// TODO: ensure instructions.length < max bank size
 case class Bank(var instructions: ArrayBuffer[Instruction] = ArrayBuffer[Instruction]())
 
 // TODO: replace instances of banks with Program
