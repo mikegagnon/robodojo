97051b6b1dde492c6f93db07dc18574ae6132427
Michael Gagnon <mikegagnon@gmail.com>
2016-06-19 16:31:39 -0600

    Uncaught Error; preparing to debug

--- src/main/scala/club/robodojo/Robodojo.scala
+++ src/main/scala/club/robodojo/Robodojo.scala
@@ -7,7 +7,7 @@ import scala.util.Random
 class Robodojo(preload: createjs.LoadQueue)(implicit val config: Config) {
   val board = new Board()
 
-  val density = 0.5
+/*  val density = 0.5
 
   val rand = new Random(0)
 
@@ -43,24 +43,14 @@ class Robodojo(preload: createjs.LoadQueue)(implicit val config: Config) {
           board.addBot(bot)
       }
     }
-  }
+  }*/
 
-  /*
-  val bot1 = Bot(board, 1, 0)
-  bot1.direction = Direction.Right
-  val bank0 = new Bank()
-  bank0.instructions :+= MoveInstruction()
-  //bank0.instructions :+= TurnInstruction(0)
-  bot1.banks += (0 -> bank0)
+  
+
+  val bot1Program = Compiler.compile("move\nmove\nturn 1").right
+  val bot1 = Bot(board, PlayerColor.Blue, 1, 0, Direction.Right)
   board.addBot(bot1)
-
-  val bot2 = Bot(board, 4, 6)
-  bot2.direction = Direction.Left
-  board.addBot(bot2)
-
-  val bot3 = Bot(board, 2, 4)
-  bot3.direction = Direction.Down
-  board.addBot(bot3)*/
+  
 
   val viz = new Viz(preload, board)
 
