8b4e2001e0ed9560e77d9742da144322fee7b4fe
Michael Gagnon <mikegagnon@gmail.com>
2016-06-23 14:42:25 -0600

    Moved instruction constants and params to top and have ParamValue extend Param

--- src/main/scala/club/robodojo/Instruction.scala
+++ src/main/scala/club/robodojo/Instruction.scala
@@ -20,6 +20,35 @@ sealed abstract class Instruction {
   def cycle(bot: Bot, cycleNum: Int) : Option[Animation]
 }
 
+object Constant {
+  sealed trait EnumVal
+  case object Banks extends EnumVal
+  case object Mobile extends EnumVal
+  case object InstrSet extends EnumVal
+  case object Fields extends EnumVal
+}
+
+case class ActiveVariable()
+
+sealed trait Param
+sealed trait ParamValue extends Param // Like Param, but without Label
+
+final case class Integer(value: Short) extends ParamValue
+// TODO: should Label be under Param?
+final case class Label(value: String) extends Param
+final case class Constant(value: Constant.EnumVal) extends ParamValue
+final case class Remote(value: Constant.EnumVal) extends ParamValue
+final case class Variable(value: Either[Int, ActiveVariable])(implicit config: Config)
+    extends ParamValue {
+  value match {
+    case Left(v) => if (v < 0 || v >= config.sim.maxNumVariables) {
+      throw new IllegalArgumentException("variable value out of range: " + v)
+    }
+    case _ => ()
+  }
+}
+
+
 // TODO: move appropriate functions into objects
 
 case class MoveInstruction(implicit val config: Config) extends Instruction {
@@ -54,34 +83,6 @@ case class MoveInstruction(implicit val config: Config) extends Instruction {
       }
       case Some(_) => Some(MoveAnimationFail(bot.id))
     }
-  }
-}
-
-object Constant {
-  sealed trait EnumVal
-  case object Banks extends EnumVal
-  case object Mobile extends EnumVal
-  case object InstrSet extends EnumVal
-  case object Fields extends EnumVal
-}
-
-case class ActiveVariable()
-
-sealed trait Param
-sealed trait ParamValue // Like Param, but without Label
-
-final case class Integer(value: Short) extends Param with ParamValue
-// TODO: should Label be under Param?
-final case class Label(value: String) extends Param
-final case class Constant(value: Constant.EnumVal) extends Param with ParamValue
-final case class Remote(value: Constant.EnumVal) extends Param with ParamValue
-final case class Variable(value: Either[Int, ActiveVariable])(implicit config: Config) extends Param
-    with ParamValue {
-  value match {
-    case Left(v) => if (v < 0 || v >= config.sim.maxNumVariables) {
-      throw new IllegalArgumentException("variable value out of range: " + v)
-    }
-    case _ => ()
   }
 }
 
@@ -256,3 +257,4 @@ case class CreateInstruction(
     }
   }
 }
+
