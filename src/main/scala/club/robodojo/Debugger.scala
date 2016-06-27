2fe7e5f1fa36748e12591d3e47abcba642eb99ae
Michael Gagnon <mikegagnon@gmail.com>
2016-06-27 11:30:59 -0600

    Shell for Debugger addHtml

--- src/main/scala/club/robodojo/Debugger.scala
+++ src/main/scala/club/robodojo/Debugger.scala
@@ -1,5 +1,23 @@
 package club.robodojo
 
+import org.scalajs.jquery.jQuery
+
 class Debugger(val controller: Controller, val viz: Viz)(implicit val config: Config) {
+
+  /** Begin initialization ************************************************************************/
+
+  addHtml()
+
+  /** End initialization **************************************************************************/
+
+  def addHtml(): Unit = {
+
+    val html = s"""
+      <div class="debugger">
+      </div>
+      """
+
+    jQuery("#" + config.id).append(html)
+  }
 
 }
