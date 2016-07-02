2c600e89e72c7aa18263b72685ccc94382b49f50
Michael Gagnon <mikegagnon@gmail.com>
2016-07-02 10:54:32 -0600

    Breakpoint symbol appears and disappears on click

--- src/main/scala/club/robodojo/CodeMirrorDojo.scala
+++ src/main/scala/club/robodojo/CodeMirrorDojo.scala
@@ -1,19 +1,44 @@
 package club.robodojo
 
 import org.denigma.codemirror.extensions.EditorConfig
+import org.denigma.codemirror
 import org.denigma.codemirror.{CodeMirror, EditorConfiguration}
 import org.scalajs.dom
+import org.scalajs.dom.raw.HTMLElement
 import org.scalajs.dom.raw.HTMLTextAreaElement
+import scala.scalajs.js
 
 object CodeMirrorDojo {
 
-  def getCmEditor(readOnly: Boolean, textAreaId: String): org.denigma.codemirror.Editor = {
+  def getCmEditor(readOnly: Boolean, textAreaId: String): codemirror.Editor = {
     val mode = "clike"
-    val params: EditorConfiguration = EditorConfig.mode(mode).lineNumbers(true).readOnly(readOnly)
+    val params: EditorConfiguration = EditorConfig
+      .mode(mode)
+      .lineNumbers(true)
+      .readOnly(readOnly)
+      .gutters(js.Array("CodeMirror-linenumbers", "breakpoints"))
 
-    dom.document.getElementById(textAreaId) match {
+    val editor = dom.document.getElementById(textAreaId) match {
       case el:HTMLTextAreaElement => CodeMirror.fromTextArea(el,params)
       case _=> throw new IllegalStateException("Could not find textarea for " + textAreaId)
     }
+
+    // https://codemirror.net/demo/marker.html
+    editor.on("gutterClick", (cm: codemirror.Editor, n: Int) => {
+      val info = cm.lineInfo(n)
+      val gutterMarkers: js.UndefOr[js.Array[String]] = info.gutterMarkers
+      val gutterMarkersOpt = gutterMarkers.toOption
+      cm.setGutterMarker(n, "breakpoints", if (gutterMarkers.nonEmpty) null else makeMarker()) //if (info.gutterMarkers.nonEmpty) null else makeMarker())
+      ()
+    })
+
+    editor
+  }
+
+  def makeMarker(): HTMLElement = {
+    var marker = dom.document.createElement("div").asInstanceOf[HTMLElement] 
+    marker.style.color = "#822"
+    marker.innerHTML = "●"
+    return marker
   }
 }
